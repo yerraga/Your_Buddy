88258e13d8f65c8247f98aa313c5439a51cc124a
Yerragorla Ganesh Yadav <yerragorlaganeshyadav@Yerragorlas-MacBook-Pro.local>
2024-03-28 02:00:26 +0530

    basic coding has been done for initial commit

---
+++ src/main/java/com/yourBuddy/YourBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.yourBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourBuddyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yourBuddy/entity/User.java
@@ -0,0 +1,25 @@
+package com.yourBuddy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+}
