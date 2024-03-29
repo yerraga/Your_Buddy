8d8d9d60102d7b97968b2d564c4e11786be829f0
Yerragorla Ganesh Yadav <yerragorlaganeshyadav@Yerragorlas-MacBook-Pro.local>
2024-03-30 03:11:43 +0530

    implemented code for validation

--- src/main/java/com/yourBuddy/entity/User.java
+++ src/main/java/com/yourBuddy/entity/User.java
@@ -2,6 +2,8 @@ package com.yourBuddy.entity;
 
 import com.yourBuddy.enums.UserType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,12 +19,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull (message = "First name cannot be empty")
     private String firstName;
 
+    @NotNull (message = "Last name cannot be empty")
     private String lastName;
 
+    @Email(message = "Invalid email")
+    @NotNull (message = "Email cannot be empty")
     private String email;
 
+    @NotNull (message = "User type cannot be empty")
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
--- src/main/java/com/yourBuddy/enums/UserType.java
+++ src/main/java/com/yourBuddy/enums/UserType.java
@@ -3,5 +3,6 @@ package com.yourBuddy.enums;
 public enum UserType {
 
     USER,
-    MERCHANT
+    MERCHANT,
+    NA
 }
